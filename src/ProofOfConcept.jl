357f8a92829f9287a90e682fcf1a39a62488b9cc
Albert Oliver <albert.oliver@ulpgc.es>
2021-11-01 17:03:27 +0000

    Implementation of the proof of concept

--- src/ProofOfConcept.jl
+++ src/ProofOfConcept.jl
@@ -1,5 +1,159 @@
 module ProofOfConcept
 
+using LinearAlgebra
+
+import Base: *, ==
+
+export Triangle
+export Point
+export get_barycentric_coordinates
+export *
+export ==
+export is_inside
+export refinement_criteria
+export TerrainMap
+export look_for_points_inside_triangle
+export distribute_and_mark_for_refinement
+
 # Write your package code here.
+struct Point
+    coords::Vector{Real}
+    height::Real
+end
+
+function Point(coords)
+    return Point(coords, NaN)
+end
+
+function get_coords(p::Point)
+    return p.coords
+end
+
+function ==(p::Point, v)
+return all(p.coords ≈ v)
+end
+
+struct Triangle
+    coords::Matrix{Real}
+    height::Vector{Real}
+end
+
+function Triangle(m)
+    t = Triangle(m, similar(m, length(m)))
+end
+
+function Triangle(p1, p2, p3)
+    return Triangle([p1 p2 p3])
+end
+
+function Triangle(p1::Point, p2, p3)
+    return Triangle([get_coords(p1) p2 p3])
+end
+
+function get_v1(t)
+    return t.coords[:,1]
+end
+
+function get_v2(t)
+    return t.coords[:,2]
+end
+
+function get_v3(t)
+    return t.coords[:,3]
+end
+
+function get_area(t)
+    t_v1 = get_v2(t) - get_v1(t)
+    t_v2 = get_v3(t) - get_v1(t)
+
+    return det([t_v1 t_v2])/2
+end
+
+
+function get_barycentric_coordinates(p, t)
+    t_area = get_area(t)
+    t1 = Triangle(p, get_v2(t), get_v3(t))
+    t1_area = get_area(t1)
+    t2 = Triangle(p, get_v3(t), get_v1(t))
+    t2_area = get_area(t2)
+    t3 = Triangle(p, get_v1(t), get_v2(t))
+    t3_area = get_area(t3)
+
+    return [t1_area/t_area, t2_area/t_area, t3_area/t_area]
+end
+
+function ⪅(a, b)
+return (a < b) || (a ≈ b)
+end
+
+function is_inside(bc)
+    return all(0 .⪅ bc .⪅ 1)
+end
+
+
+function is_inside(p::Point, t::Triangle)
+    bc = get_barycentric_coordinates(p, t)
+    return is_inside(bc)
+end
+
+function *(bc, t::Triangle)
+return t.coords * bc 
+end
+
+function refinement_criteria(bc, p::Point, t::Triangle, tol::Real)
+    t_height = dot(bc, t.height)
+    return abs(p.height - t_height) > tol
+end
+
+function refinement_criteria(p::Point, t::Triangle, tol::Real)
+    bc = get_barycentric_coordinates(p, t)
+    return refinement_criteria(bc, p, t, tol)
+end
+
+# The map starts at the point (lon_min, lat_min)
+# and moves first on longitude and then on latitude
+struct TerrainMap
+    lat_min::Real
+    lon_min::Real
+    Δ_lat::Real
+    Δ_lon::Real
+    height::Matrix{Real}
+end
+
+function get_point(m::TerrainMap, idx::CartesianIndex)
+    lon = m.lon_min + (idx[1] - 1) * m.Δ_lon
+    lat = m.lat_min + (idx[2] - 1) * m.Δ_lat
+    height = m.height[idx]
+    return Point([lon, lat], height)
+end
+
+function get_point(idx::CartesianIndex, m::TerrainMap)
+    return get_point(m, idx)
+end
+
+function is_inside(m::TerrainMap, idx::CartesianIndex, t::Triangle)
+    return is_inside(get_point(m, idx), t)
+end
+
+function refinement_criteria(m::TerrainMap, idx::CartesianIndex, t::Triangle, tol::Real)
+    return refinement_criteria(get_point(m, idx), t, tol)
+end
+
+function look_for_points_inside_triangle(m::TerrainMap, t::Triangle)
+    return idx -> is_inside(m, idx, t)
+end
+
+function get_barycentric_coordinates(m::TerrainMap, idx::CartesianIndex, t::Triangle)
+    return get_barycentric_coordinates(get_point(m, idx), t)
+end
+
+function distribute_and_mark_for_refinement(m::TerrainMap, parent_indices, son_t::Triangle, tol::Real)
+    pts = map(x -> get_point(m, x), parent_indices)
+    bc = map(x -> get_barycentric_coordinates(x, son_t), pts)
+    local_indices = findall(is_inside, bc)
+    son_indices = view(parent_indices, local_indices)
+    refine = any(i -> refinement_criteria(bc[i], pts[i], son_t, tol), local_indices)
+    return (refine, son_indices)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,119 @@
 using ProofOfConcept
 using Test
 
-@testset "ProofOfConcept.jl" begin
-    # Write your tests here.
+@testset "Barycentric coordinates" begin
+    t = Triangle([0, 0], [1, 0], [0, 1])
+    p1 = Point([0.1, 0.1])
+    bc = get_barycentric_coordinates(p1, t)
+    @test p1 == bc * t
+
+    p2 = Point([0.2, 0.1])
+    bc = get_barycentric_coordinates(p2, t)
+    @test p2 == bc * t
+
+    p3 = Point([0.8, 0.1])
+    bc = get_barycentric_coordinates(p3, t)
+    @test p3 == bc * t
+
+    p4 = Point([1.8, -2.1])
+    bc = get_barycentric_coordinates(p4, t)
+    @test p4 == bc * t
+end
+
+@testset "is_inside" begin
+    t = Triangle([0, 0], [1, 0], [0, 1])
+
+
+    p1 = Point([0.1, 0.1])
+    @test is_inside(p1, t)
+    bc = get_barycentric_coordinates(p1, t)
+    @test is_inside(bc)
+
+    p2 = Point([0.2, 0.1])
+    @test is_inside(p2, t)
+    bc = get_barycentric_coordinates(p2, t)
+    @test is_inside(bc)
+
+    p3 = Point([0.8, 0.1])
+    @test is_inside(p3, t)
+    bc = get_barycentric_coordinates(p3, t)
+    @test is_inside(bc)
+
+    p4 = Point([1.8, -2.1])
+    @test !is_inside(p4, t)
+    bc = get_barycentric_coordinates(p4, t)
+    @test !is_inside(bc)
+end
+
+@testset "Refinement criteria" begin
+    t = Triangle([[0, 0] [1, 0] [0, 1]], [1, 2, 3])
+    tol = 0.1
+
+    p1 = Point([0.1, 0.1], 5)
+    @test refinement_criteria(p1, t, tol)
+
+    p1 = Point([0, 0], 1.09)
+    @test !refinement_criteria(p1, t, tol)
+
+end
+
+@testset "Distribute points" begin
+
+    h = reshape(collect(1:6*5), 6, 5)
+    m = TerrainMap(0, 0, 10, 10, h)
+    idx = CartesianIndices(h)
+    
+    t1 = Triangle([[15, 15] [45, 0] [20, 40]], [1, 2, 3])
+    map_t1 = view(idx, findall(look_for_points_inside_triangle(m, t1), idx))
+
+    @test all(map_t1 == [CartesianIndex(4, 2), CartesianIndex(3, 3), CartesianIndex(4, 3), CartesianIndex(3, 4), CartesianIndex(3, 5)])
+
+    # Now we are splitting the triangle
+    t2 = Triangle([[15, 15] [35, 15] [20, 40]], [1, 2, 3])
+    map_t2 = view(map_t1, findall(look_for_points_inside_triangle(m, t2), map_t1))
+
+    @test all(map_t2 == [CartesianIndex(3, 3), CartesianIndex(4, 3), CartesianIndex(3, 4), CartesianIndex(3, 5)])
+
+    t3 = Triangle([[15, 15] [45, 0] [35, 15]], [1, 2, 3])
+    map_t3 = view(map_t1, findall(look_for_points_inside_triangle(m, t3), map_t1))
+
+    @test all(map_t3 == [CartesianIndex(4, 2)])
+
+end
+
+@testset "Distribute and mark for refinement" begin
+
+    tol = 0.1
+
+    h = reshape(collect(1:6*5), 6, 5)
+    m = TerrainMap(0, 0, 10, 10, h)
+    idx = CartesianIndices(h)
+
+    t1 = Triangle([[15, 15] [45, 0] [20, 40]], [1, 2, 3])
+    (refine_t1, map_t1) = distribute_and_mark_for_refinement(m, idx, t1, tol)
+    @test refine_t1
+    @test all(map_t1 == [CartesianIndex(4, 2), CartesianIndex(3, 3), CartesianIndex(4, 3), CartesianIndex(3, 4), CartesianIndex(3, 5)])
+
+
+    # Now we are splitting t1 into t2 and t3
+    t2 = Triangle([[15, 15] [35, 15] [20, 40]], [1, 2, 3])
+    (refine_t2, map_t2) = distribute_and_mark_for_refinement(m, map_t1, t2, tol)
+    @test refine_t2
+    @test all(map_t2 == [CartesianIndex(3, 3), CartesianIndex(4, 3), CartesianIndex(3, 4), CartesianIndex(3, 5)])
+
+    t3 = Triangle([[15, 15] [45, 0] [35, 15]], [1, 2, 3])
+    (refine_t3, map_t3) = distribute_and_mark_for_refinement(m, map_t1, t3, tol)
+    @test refine_t3
+    @test all(map_t3 == [CartesianIndex(4, 2)])
+
+    # Now we are splitting t2 into t4 and t5
+    t4 = Triangle([[15, 15] [25, 15] [20, 40]], [1, 2, 3])
+    (refine_t4, map_t4) = distribute_and_mark_for_refinement(m, map_t2, t4, tol)
+    @test refine_t4
+    @test all(map_t4 == [CartesianIndex(3, 3), CartesianIndex(3, 4), CartesianIndex(3, 5)])
+
+    t5 = Triangle([[25, 15] [35, 15] [20, 40]], [1, 2, 3])
+    (refine_t5, map_t5) = distribute_and_mark_for_refinement(m, map_t2, t5, tol)
+    @test refine_t5
+    @test all(map_t5 == [CartesianIndex(4, 3), CartesianIndex(3, 5)])
 end
